471f1a449f964360e8173b045300473db8c7d96c
Cameron Pitt-Downton <cameron@positron.com.au>
2019-08-21 15:17:13 +1000

    Now passes flake8 as well.

--- event_listener.py
+++ event_listener.py
@@ -1,8 +1,6 @@
 import os
 import gazu
-import partd
 import shutil
-import logging
 
 from avalon import io as avalon
 import lib as lib
@@ -14,6 +12,7 @@ os.environ["AVALON_SILO"] = "assets"
 os.environ["AVALON_TASK"] = "model"
 os.environ["AVALON_WORKDIR"] = "/avalon"
 
+
 def asset_create_callback(data):
     """
     On receiving a asset:create event, insert the asset into the 
@@ -24,7 +23,7 @@ def asset_create_callback(data):
     # Log in to API
     gazu.client.set_host("{0}/api".format(os.environ["GAZU_URL"]))
     gazu.log_in(os.environ["GAZU_USER"], os.environ["GAZU_PASSWD"])
-    
+
     asset = gazu.asset.get_asset(data["asset_id"])
     project = gazu.project.get_project(asset["project_id"])
 
@@ -54,15 +53,15 @@ def asset_create_callback(data):
     # Get the Id of the asset we just inserted into Avalon
     avalon_asset = avalon.find_one(
         {"name": lib.get_consistent_name(asset["name"]),
-        "type": "asset"})
+            "type": "asset"})
 
     # Encode and store the Gazu Id and Avalon Id
     lib.set_asset_data(project["id"], data["asset_id"], avalon_asset["_id"])
 
     avalon.uninstall()
 
-    logger.info("Create Asset \"{0}\" in Project \"{1}\"".format(asset["name"], 
-        project["name"]))
+    logger.info("Create Asset \"{0}\" in Project \"{1}\"".format(
+        asset["name"], project["name"]))
 
 
 def asset_update_callback(data):
@@ -70,7 +69,7 @@ def asset_update_callback(data):
     # Log in to API
     gazu.client.set_host("{0}/api".format(os.environ["GAZU_URL"]))
     gazu.log_in(os.environ["GAZU_USER"], os.environ["GAZU_PASSWD"])
-    
+
     asset = gazu.asset.get_asset(data["asset_id"])
     project = gazu.project.get_project(asset["project_id"])
     project_name = lib.get_consistent_name(project["name"])
@@ -88,7 +87,7 @@ def asset_update_callback(data):
     # Find the asset in Avalon
     avalon_asset = avalon.find_one(
         {"_id": avalon.ObjectId(asset_id),
-        "type": "asset"})
+            "type": "asset"})
 
     # Set keep asset name for use in filesystem path renaming.
     old_asset_name = lib.get_consistent_name(avalon_asset["name"])
@@ -105,14 +104,15 @@ def asset_update_callback(data):
 
     avalon.uninstall()
 
-    logger.info("Updated Asset \"{0}\" in Project \"{1}\"".format(old_asset_name, 
-        project["name"]))
+    logger.info("Updated Asset \"{0}\" in Project \"{1}\"".format(
+        old_asset_name, project["name"]))
 
     if asset_name != old_asset_name:
-        logger.info("Asset renamed from \"{0}\" to \"{1}\"".format(old_asset_name, 
-        asset_name))
-    
-    # If file system path renaming is enabled, rename asset disk filepaths to match.
+        logger.info("Asset renamed from \"{0}\" to \"{1}\"".format(
+            old_asset_name, asset_name))
+
+    # If file system path renaming is enabled, rename asset disk 
+    # filepaths to match.
     if(os.environ["FILESYS_RENAME"]):
         lib.rename_filepath(old_asset_name, asset_name, project_name, "assets")
 
@@ -145,8 +145,10 @@ def project_new_callback(data):
         resolution_width = None
         project["resolution"] = None
     # Get tasks from Gazu API
-    tasks = [{"name": lib.get_consistent_name(task["name"]), 
-        "label":task["name"]} for task in gazu.task.all_task_types()]
+    tasks = [
+        {"name": lib.get_consistent_name(task["name"]), 
+            "label":task["name"]} 
+        for task in gazu.task.all_task_types()]
 
     project_data = {
         "schema": "avalon-core:project-2.0",
@@ -185,11 +187,11 @@ def project_new_callback(data):
     # Find the project in Avalon
     avalon_project = avalon.find_one(
         {"name": lib.get_consistent_name(project["name"]),
-        "type": "project"})
+            "type": "project"})
 
     # Encode and store the data
-    lib.set_project_data(data["project_id"], avalon_project["_id"],
-        avalon_project['name'])
+    lib.set_project_data(
+        data["project_id"], avalon_project["_id"], avalon_project['name'])
 
     avalon.uninstall()
 
@@ -201,7 +203,7 @@ def project_update_callback(data):
     # Log in to API
     gazu.client.set_host("{0}/api".format(os.environ["GAZU_URL"]))
     gazu.log_in(os.environ["GAZU_USER"], os.environ["GAZU_PASSWD"])
-    
+
     project = gazu.project.get_project(data["project_id"])
 
     # Get the Avalon project ID from partd
@@ -215,12 +217,12 @@ def project_update_callback(data):
     # Find the project in Avalon
     avalon_project = avalon.find_one(
         {"_id": avalon.ObjectId(project_data["id"]),
-        "type": "project"})
+            "type": "project"})
 
     # Ensure project["name"] consistency.
     project_name = lib.get_consistent_name(project["name"])
     old_project_name = lib.get_consistent_name(avalon_project["name"])
-    
+
     # Projects may not have a resolution set
     if project["resolution"]:
         resolution_width = int(int(project["resolution"]) / 9 * 16)
@@ -229,8 +231,10 @@ def project_update_callback(data):
         project["resolution"] = None
 
     # Get latest Tasks from Gazu
-    tasks = [{"name": lib.get_consistent_name(task["name"]), 
-        "label":task["name"]} for task in gazu.task.all_task_types()]
+    tasks = [
+        {"name": lib.get_consistent_name(task["name"]), 
+            "label":task["name"]} 
+        for task in gazu.task.all_task_types()]
 
     # Update the Avalon project with new data from Gazu
     avalon_project["name"] = project_name
@@ -242,19 +246,18 @@ def project_update_callback(data):
 
     avalon.replace_one(
         {"_id": avalon.ObjectId(project_data["id"]),
-        "type": "project"}, avalon_project
+            "type": "project"}, avalon_project
     )
-    
+
     avalon.uninstall()
 
-    
     if old_project_name != project_name:
         logger.info("Updating project name from {0} to {1}".format(
             old_project_name, project_name))
         lib.collection_rename(project_name)
 
-        lib.set_project_data(data["project_id"], avalon_project["_id"],
-            avalon_project["name"])
+        lib.set_project_data(
+            data["project_id"], avalon_project["_id"], avalon_project["name"])
 
         if(os.environ["FILESYS_RENAME"]):
             avalon_projects = os.environ["AVALON_PROJECTS"]
@@ -265,17 +268,22 @@ def project_update_callback(data):
 
             if os.path.exists(old_folder_name):
                 if not os.path.exists(new_folder_name):
-                    logger.info("Project name updated, renaming {0} to {1}"
-                        .format(old_folder_name, new_folder_name))
+                    logger.info("Project name updated, "
+                                "renaming {0} to {1}".format(
+                                    old_folder_name, new_folder_name))
                     shutil.move(old_folder_name, new_folder_name)
                 else:
-                    logger.warning("Project name updated, trying to rename {0} to {1}, "
-                        "but new folder already exists. No action taken"
+                    logger.warning(
+                        "Project name updated, trying to "
+                        "rename {0} to {1}, but new folder "
+                        "already exists. No action taken"
                         .format(old_folder_name, new_folder_name)
                     )
             else:
-                logger.warning("Project name updated, but {0} does not exist. No action taken"
-                        .format(old_folder_name))
+                logger.warning(
+                    "Project name updated, but {0} does not exist. No "
+                    "action taken"
+                    .format(old_folder_name))
 
     logger.info("Updating Project: \"{0} ({1})\"".format(
         avalon_project["data"]["label"], project_name))
@@ -299,8 +307,8 @@ def shot_new_callback(data):
     episode_name = lib.get_consistent_name(shot["episode_name"])
     sequence_name = lib.get_consistent_name(shot["sequence_name"])
     shot_name = lib.get_consistent_name(shot["name"])
-    visualParent = [project_name, "{0}_{1}".format(episode_name, 
-                sequence_name)]
+    visualParent = [project_name, "{0}_{1}".format(
+        episode_name, sequence_name)]
 
     os.environ["AVALON_PROJECT"] = project_name
 
@@ -315,7 +323,8 @@ def shot_new_callback(data):
         "parent": avalon.locate([project_name]),
         "data": {
             "label": shot["name"],
-            "group": "{0} {1}".format(shot["episode_name"].upper(), 
+            "group": "{0} {1}".format(
+                shot["episode_name"].upper(), 
                 shot["sequence_name"].upper()),
             "visualParent": avalon.locate(visualParent)
         }
@@ -327,13 +336,13 @@ def shot_new_callback(data):
     # Get the Id of the shot we just inserted into Avalon
     avalon_shot = avalon.find_one(
         {"name": lib.get_consistent_name(shot_data["name"]),
-        "type": "asset"})
+            "type": "asset"})
 
     # Encode and store the Gazu Id and Avalon Id
     lib.set_asset_data(project["id"], data["shot_id"], avalon_shot["_id"])
 
-    logger.info("Created Shot \"{0}\" in Project \"{1}\"".format(shot["name"], 
-        project["name"]))
+    logger.info("Created Shot \"{0}\" in Project \"{1}\"".format(
+        shot["name"], project["name"]))
 
 
 def shot_update_callback(data):
@@ -341,7 +350,7 @@ def shot_update_callback(data):
     # Log in to API
     gazu.client.set_host("{0}/api".format(os.environ["GAZU_URL"]))
     gazu.log_in(os.environ["GAZU_USER"], os.environ["GAZU_PASSWD"])
-    
+
     shot = gazu.shot.get_shot(data["shot_id"])
     project = gazu.project.get_project(shot["project_id"])
 
@@ -350,8 +359,8 @@ def shot_update_callback(data):
     episode_name = lib.get_consistent_name(shot["episode_name"])
     sequence_name = lib.get_consistent_name(shot["sequence_name"])
     shot_name = lib.get_consistent_name(shot["name"])
-    visualParent = [project_name, "{0}_{1}".format(episode_name, 
-                sequence_name)]
+    visualParent = [project_name, "{0}_{1}".format(
+        episode_name, sequence_name)]
 
     os.environ["AVALON_PROJECT"] = project_name
 
@@ -364,19 +373,20 @@ def shot_update_callback(data):
     # Find the asset in Avalon
     avalon_shot = avalon.find_one(
         {"_id": avalon.ObjectId(shot_id),
-        "type": "asset"})
+            "type": "asset"})
 
     # Set keep shot name for use in filesystem path renaming.
     old_shot_name = lib.get_consistent_name(avalon_shot["name"])
-    new_shot_name = "{0}_{1}_{2}".format(episode_name, sequence_name, shot_name)
+    new_shot_name = "{0}_{1}_{2}".format(
+        episode_name, sequence_name, shot_name)
 
     avalon_shot["name"] = new_shot_name
     avalon_shot["data"]["label"] = shot["name"]
-    avalon_shot["data"]["group"] = "{0} {1}".format(shot["episode_name"].upper(), 
-                shot["sequence_name"].upper())
+    avalon_shot["data"]["group"] = "{0} {1}".format(
+        shot["episode_name"].upper(), shot["sequence_name"].upper())
     avalon_shot["data"]["visualParent"] = avalon.locate(visualParent)
 
-    if shot["data"] != None:
+    if shot["data"] is not None:
         if "frame_in" in shot["data"]:
             avalon_shot["data"]["edit_in"] = shot["data"]["frame_in"]
             avalon_shot["data"]["startFrame"] = shot["data"]["frame_in"]
@@ -386,20 +396,22 @@ def shot_update_callback(data):
 
     avalon.replace_one(
         {"_id": avalon.ObjectId(shot_id),
-        "type": "asset"}, avalon_shot)
+            "type": "asset"}, avalon_shot)
 
     avalon.uninstall()
 
-    logger.info("Updated Shot \"{0}\" in Project \"{1}\"".format(avalon_shot["name"], 
-        project["name"]))
+    logger.info("Updated Shot \"{0}\" in Project \"{1}\"".format(
+        avalon_shot["name"], project["name"]))
 
     if shot_name != old_shot_name:
-        logger.info("Shot renamed from \"{0}\" to \"{1}\"".format(old_shot_name, 
-        new_shot_name))
-    
-    # If file system path renaming is enabled, rename asset disk filepaths to match.
+        logger.info("Shot renamed from \"{0}\" to \"{1}\"".format(
+            old_shot_name, new_shot_name))
+
+    # If file system path renaming is enabled, rename asset disk 
+    # filepaths to match.
     if(os.environ["FILESYS_RENAME"]):
-        lib.rename_filepath(old_shot_name, new_shot_name, project_name, "shots")
+        lib.rename_filepath(
+            old_shot_name, new_shot_name, project_name, "shots")
 
 
 def task_new_callback(data):
@@ -431,9 +443,9 @@ def task_new_callback(data):
     # Find the asset in Avalon
     avalon_entity = avalon.find_one(
         {"_id": avalon.ObjectId(entity_id),
-        "type": "asset"})
+            "type": "asset"})
 
-    if avalon_entity["data"] != None:
+    if avalon_entity["data"] is not None:
         if "tasks" in avalon_entity["data"]:
             avalon_entity["data"]["tasks"].append(task_name)
         else:
@@ -446,8 +458,8 @@ def task_new_callback(data):
 
     avalon.uninstall()
 
-    logger.info("Added new \"{2}\" Task to \"{0}\" in Project \"{1}\""
-        .format(avalon_entity["name"], project["name"], task_type["name"]))
+    logger.info("Added new \"{2}\" Task to \"{0}\" in Project \"{1}\"".format(
+        avalon_entity["name"], project["name"], task_type["name"]))
 
 
 # Init Logging
@@ -463,7 +475,8 @@ gazu.events.add_listener(event_client, "asset:update", asset_update_callback)
 
 # Project Event Types
 gazu.events.add_listener(event_client, "project:new", project_new_callback)
-gazu.events.add_listener(event_client, "project:update", project_update_callback)
+gazu.events.add_listener(
+    event_client, "project:update", project_update_callback)
 
 # Shot Event Types
 gazu.events.add_listener(event_client, "shot:new", shot_new_callback)
